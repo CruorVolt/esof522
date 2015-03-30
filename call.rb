afc030c6db5dd047e36b4944176c0a56a8905740
Anders Lundgren <lundgren.am@gmail.com>
2015-03-29 22:27:49 -0600

    Compacted with Procs for API, forks instead of stars

--- call.rb
+++ call.rb
@@ -6,51 +6,40 @@ REPOS_LIST = "repos.txt"
 client = Octokit::Client.new(:access_token => API_KEY)
 client.auto_paginate = true #auto rate-limiting and page concat (theoretically)
 
+issues_call = Proc.new do |repo| client.list_issues(repo, :state => "all", :sort => "created") end
+issues_time = Proc.new do |issue| issue.created_at.to_i end
+
+commits_call = Proc.new do |repo| client.list_commits(repo) end
+commits_time = Proc.new do |commit| commit.commit.author.date.to_i end
+
+forks_call = Proc.new do |repo| client.forks(repo) end
+forks_time = issues_time
+
+def write_api_results(call_proc, time_proc, repo_name, header = ["total","time"], dir_path = Dir.pwd, type = "")
+	repo = repo_name.split("/")[1] #seperate name from user name
+	File.open(File.join(dir_path, "#{repo}#{type}.csv"), "w") do |file|
+		results = call_proc.call repo_name
+		count = results.length
+		file.puts header.join(",")
+		results.each do |result|
+			time = time_proc.call result
+			file.puts "#{count},#{time}"
+			count = count - 1
+		end
+		
+	end
+end
+
 File.open REPOS_LIST do |repos|
 	repos.each do |repo|
 		repo.chomp!
 		file_path = File.join(Dir.pwd, "api_results")
-
-		#Issues call
-		issues_file = "#{repo.split("/")[1].chomp}_issues.csv"
-		File.open(File.join(file_path, issues_file), "w") do |new_file|
-			issues = client.list_issues(repo, :state => "all", :sort => "created") #newest-first
-			num = issues.length
-			puts "Found #{num} issues for #{repo}"
-			new_file.puts "issue_id, total, time"
-			issues.each do |issue|
-				new_file.puts  "#{issue.id}, #{num}, #{issue.created_at.to_i}"
-				num = num -1 #count down to issue-0
-			end
-		end
-
-		#Commits call
-		commits_file = "#{repo.split("/")[1].chomp}_commits.csv"
-		File.open(File.join(file_path, commits_file), "w") do |new_file|
-			commits = client.list_commits(repo) #newest first
-			num = commits.length
-			puts "Found #{num} commits for #{repo}"
-			new_file.puts "commit_sha, total, time"
-			commits.each do |commit|
-				new_file.puts  "#{commit.sha}, #{num}, #{commit.commit.author.date.to_i}" 
-				num = num -1 #count down to initial commit
-			end
-		end
-
-=begin
-		#Stars call
-		stars_file = "#{repo.split("/")[1].chomp}_stars.csv"
-		File.open(File.join(file_path, stars_file), "w") do |new_file|
-			stars = client.stargazers(repo, :accept => "application/vnd.github.v3.star+json")
-			num = stars.length
-			puts "Found #{num} stars for #{repo}"
-			new_file.puts "total, time"
-			stars.each do |star|
-				new_file.puts  "#{num}, #{star.starred_at}" 
-				num = num -1 #count down to initial commit
-			end
-		end
-=end
-
+		puts "starting"
+		write_api_results(issues_call, issues_time, repo, ["total", "time"], file_path, "_issues")
+		puts "done with issues for #{repo}"
+		write_api_results(commits_call, commits_time, repo, ["total", "time"], file_path, "_commits")
+		puts "done with commits for #{repo}"
+		write_api_results(forks_call, forks_time, repo, ["total", "time"], file_path, "_forks")
+		puts "done with forks for #{repo}"
 	end
 end
