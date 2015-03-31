43f1e952a4d5999b3d3c7d1cf2669f00424dc7a3
Anders Lundgren <lundgren.am@gmail.com>
2015-03-31 16:38:54 -0600

    Check correctly ids directories and clones as needed

--- check.rb
+++ check.rb
@@ -3,9 +3,14 @@ REPOS_LIST = "repos.txt"
 def check(dir_path = Dir.pwd)
 	File.open REPOS_LIST do |repos|
 		repos.each do |repo_name|
+			repo_name.chomp!
 			repo = repo_name.split("/")[1] #seperate name from GitHub user name
+			puts "found repo: #{repo}, checking directory #{File.join(dir_path, "checkstyle_output", repo)}"
 			if !(Dir.exist? File.join(dir_path, "checkstyle_output", repo)) #clone if repo doesn't exist
-				system("git clone https://github.com/#{repo_name}.git #{File.join(dir_path, 'checkstyle_output')}")
+				puts "That directory doesn't exist, running git clone https://github.com/#{repo_name}.git #{File.join(dir_path, 'checkstyle_output')}"
+				system("git clone https://github.com/#{repo_name}.git #{File.join(dir_path, 'checkstyle_output', repo)}")
+			else 
+				puts "That directory already exists"
 			end
 			#list commits
 			#prep output file
@@ -18,3 +23,4 @@ def check(dir_path = Dir.pwd)
 	end
 end
 
+check
