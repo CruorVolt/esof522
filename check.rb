8b3976122f5f3d7f3aef675f1efb6a9b98e2517f
Anders Lundgren <lundgren.am@gmail.com>
2015-03-31 16:56:54 -0600

    Lists commits for each pulled repo
    Don't go adding /checkstyle_output subfolders
    to this repo, they've got .git folders now

--- check.rb
+++ check.rb
@@ -5,14 +5,20 @@ def check(dir_path = Dir.pwd)
 		repos.each do |repo_name|
 			repo_name.chomp!
 			repo = repo_name.split("/")[1] #seperate name from GitHub user name
-			puts "found repo: #{repo}, checking directory #{File.join(dir_path, "checkstyle_output", repo)}"
-			if !(Dir.exist? File.join(dir_path, "checkstyle_output", repo)) #clone if repo doesn't exist
+			repo_path = File.join(dir_path, "checkstyle_output", repo)
+			puts "found repo: #{repo}, checking directory #{repo_path}"
+			if !(Dir.exist? repo_path) #clone if repo doesn't exist
 				puts "That directory doesn't exist, running git clone https://github.com/#{repo_name}.git #{File.join(dir_path, 'checkstyle_output')}"
 				system("git clone https://github.com/#{repo_name}.git #{File.join(dir_path, 'checkstyle_output', repo)}")
 			else 
 				puts "That directory already exists"
 			end
 			#list commits
+			IO.popen("git --git-dir=#{repo_path}/.git --work-tree=#{repo_path} rev-list master --reverse") { |io| 
+				while (sha = io.gets) do 
+					puts "commit #{sha}"
+				end 
+			}
 			#prep output file
 			#each commit:
 				#run checkstyle
