3ce6fdf062035060ef28f7e0d411a7d5159b9f13
Anders Lundgren <lundgren.am@gmail.com>
2015-04-01 14:02:26 -0600

    Config reports all values - no threshholds sample R script for plotting out.csv

---
+++ outtest.R
@@ -0,0 +1,16 @@
+setwd("~/Documents/esof_522/project")
+out <- read.csv("out.csv")
+
+scaled <- data.frame(
+  commit = out$commit,
+  cyclo = rescale(out$cyclo, 0:1),
+  abstract = rescale(out$abstract, 0:1),
+  npath = rescale(out$npath, 0:1),
+  fanout = rescale(out$fanout, 0:1)
+)
+
+#plot(range(scaled$commit), range(0:1), type='n')
+#lines(scaled$commit, scaled$cyclo, col="red")
+#lines(scaled$commit, scaled$npath, col="blue")
+#lines(scaled$commit, scaled$abstract, col="green")
+#lines(scaled$commit, scaled$fanout, col="purple")
