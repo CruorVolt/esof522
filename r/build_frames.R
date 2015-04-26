6f594638f37bea11624773f54bea2bbc09e2a5a4
Anders Lundgren <lundgren.am@gmail.com>
2015-04-26 15:29:45 -0600

    R split - merged data in build_frames

--- r/build_frames.R
+++ r/build_frames.R
@@ -1,87 +1,134 @@
 library(plotrix)
-setwd("~/Documents/esof_522/project")
+library(plyr)
+#setwd("~/Documents/esof_522/project")
 
-ANEFacebook_quality <- read.csv("checkstyle_results/ANE-Facebook.csv")
-androidsimplefacebook_quality <- read.csv("checkstyle_results/android-simple-facebook.csv")
-dagger_quality <- read.csv("checkstyle_results/dagger.csv")
-eclipsethemes_quality <- read.csv("checkstyle_results/eclipse-themes.csv")
-facebookandroidsdk_quality <- read.csv("checkstyle_results/facebook-android-sdk.csv")
-golangideaplugin_quality <- read.csv("checkstyle_results/go-lang-idea-plugin.csv")
-guice_quality <- read.csv("checkstyle_results/guice.csv")
-jedis_quality <- read.csv("checkstyle_results/jedis.csv")
-junit_quality <- read.csv("checkstyle_results/junit.csv")
-okhttp_quality <- read.csv("checkstyle_results/okhttp.csv")
-phonegapfacebookplugin_quality <- read.csv("checkstyle_results/phonegap-facebook-plugin.csv")
-retrofit_quality <- read.csv("checkstyle_results/retrofit.csv")
-roboguice_quality <- read.csv("checkstyle_results/roboguice.csv")
+fillNAgaps <- function(x, firstBack=FALSE) {
+  ## NA's in a vector or factor are replaced with last non-NA values
+  ## If firstBack is TRUE, it will fill in leading NA's with the first
+  ## non-NA value. If FALSE, it will not change leading NA's.
+  
+  # If it's a factor, store the level labels and convert to integer
+  lvls <- NULL
+  if (is.factor(x)) {
+    lvls <- levels(x)
+    x    <- as.integer(x)
+  }
+  
+  goodIdx <- !is.na(x)
+  
+  # These are the non-NA values from x only
+  # Add a leading NA or take the first good value, depending on firstBack   
+  if (firstBack)   goodVals <- c(x[goodIdx][1], x[goodIdx])
+  else             goodVals <- c(NA,            x[goodIdx])
+  
+  # Fill the indices of the output vector with the indices pulled from
+  # these offsets of goodVals. Add 1 to avoid indexing to zero.
+  fillIdx <- cumsum(goodIdx)+1
+  
+  x <- goodVals[fillIdx]
+  
+  # If it was originally a factor, convert it back
+  if (!is.null(lvls)) {
+    x <- factor(x, levels=seq_along(lvls), labels=lvls)
+  }
+  
+  x
+}
 
-ANEFacebook_commits <- read.csv("api_results/ANE-Facebook/ANE-Facebook_commits.csv")
-ANEFacebook_forks <- read.csv("api_results/ANE-Facebook/ANE-Facebook_forks.csv")
-ANEFacebook_issues <- read.csv("api_results/ANE-Facebook/ANE-Facebook_issues.csv")
-
-androidsimplefacebook_commits <- read.csv("api_results/android-simple-facebook/android-simple-facebook_commits.csv")
-androidsimplefacebook_forks <- read.csv("api_results/android-simple-facebook/android-simple-facebook_forks.csv")
-androidsimplefacebook_issues <- read.csv("api_results/android-simple-facebook/android-simple-facebook_issues.csv")
-
-dagger_commits <- read.csv("api_results/dagger/dagger_commits.csv")
-dagger_forks <- read.csv("api_results/dagger/dagger_forks.csv")
-dagger_issues <- read.csv("api_results/dagger/dagger_issues.csv")
-
-eclipsethemes_commits <- read.csv("api_results/eclipse-themes/eclipse-themes_commits.csv")
-eclipsethemes_forks <- read.csv("api_results/eclipse-themes/eclipse-themes_forks.csv")
-eclipsethemes_issues <- read.csv("api_results/eclipse-themes/eclipse-themes_issues.csv")
-
-facebookandroidsdk_commits <- read.csv("api_results/facebook-android-sdk/facebook-android-sdk_commits.csv")
-facebookandroidsdk_forks <- read.csv("api_results/facebook-android-sdk/facebook-android-sdk_forks.csv")
-facebookandroidsdk_issues <- read.csv("api_results/facebook-android-sdk/facebook-android-sdk_issues.csv")
-
-golangideaplugin_commits <- read.csv("api_results/go-lang-idea-plugin/go-lang-idea-plugin_commits.csv")
-golangideaplugin_forks <- read.csv("api_results/go-lang-idea-plugin/go-lang-idea-plugin_forks.csv")
-golangideaplugin_issues <- read.csv("api_results/go-lang-idea-plugin/go-lang-idea-plugin_issues.csv")
-
-guice_commits <- read.csv("api_results/guice/guice_commits.csv")
-guice_forks <- read.csv("api_results/guice/guice_forks.csv")
-guice_issues <- read.csv("api_results/guice/guice_issues.csv")
-
-jedis_commits <- read.csv("api_results/jedis/jedis_commits.csv")
-jedis_forks <- read.csv("api_results/jedis/jedis_forks.csv")
-jedis_issues <- read.csv("api_results/jedis/jedis_issues.csv")
-
-junit_commits <- read.csv("api_results/junit/junit_commits.csv")
-junit_forks <- read.csv("api_results/junit/junit_forks.csv")
-junit_issues <- read.csv("api_results/junit/junit_issues.csv")
-
-okhttp_commits <- read.csv("api_results/okhttp/okhttp_commits.csv")
-okhttp_forks <- read.csv("api_results/okhttp/okhttp_forks.csv")
-okhttp_issues <- read.csv("api_results/okhttp/okhttp_issues.csv")
-
-phonegapfacebookplugin_commits <- read.csv("api_results/phonegap-facebook-plugin/phonegap-facebook-plugin_commits.csv")
-phonegapfacebookplugin_forks <- read.csv("api_results/phonegap-facebook-plugin/phonegap-facebook-plugin_forks.csv")
-phonegapfacebookplugin_issues <- read.csv("api_results/phonegap-facebook-plugin/phonegap-facebook-plugin_issues.csv")
-
-retrofit_commits <- read.csv("api_results/retrofit/retrofit_commits.csv")
-retrofit_forks <- read.csv("api_results/retrofit/retrofit_forks.csv")
-retrofit_issues <- read.csv("api_results/retrofit/retrofit_issues.csv")
-
-roboguice_commits <- read.csv("api_results/roboguice/roboguice_commits.csv")
-roboguice_forks <- read.csv("api_results/roboguice/roboguice_forks.csv")
-roboguice_issues <- read.csv("api_results/roboguice/roboguice_issues.csv")
-
-
-jedis_combined = data.frame(
-  time = jedis_quality$time,
-  cyclo = jedis_quality$cyclo,
-  npath = jedis_quality$npath,
-  fanout = jedis_quality$fanout,
-  abstract = jedis_quality$abstract,
-  combined = jedis_quality$abstract + jedis_quality$fanout + jedis_quality$npath + jedis_quality$cyclo
-)
-
-monotonic_jedis_commits <- data.frame(
-  time = order(jedis_commits$time),
-  total = jedis_commits$total
-)
-
-merged <- merge(dagger_commits, dagger_issues, by ='time', all.x='TRUE', all.y='TRUE')
+#merged = Reduce(function(...) merge(..., by='time', all=T), list(dagger_commits, dagger_forks, dagger_issues, dagger_quality))
+#merged$total.y = fillNAgaps(merged$total.y, firstBack='TRUE')
 
 #legend("topleft", c("cyclomatic complexity", "npath complexity", "data abstraction coupling", "class fanout"), lty=c(1,1,1,1), col=c("red", "blue", "purple", "green")
+
+ANEFacebook = colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/ANE-Facebook/ANE-Facebook_commits.csv"), 
+	read.csv("api_results/ANE-Facebook/ANE-Facebook_forks.csv"), 
+	read.csv("api_results/ANE-Facebook/ANE-Facebook_issues.csv"), 
+	read.csv("checkstyle_results/ANE-Facebook.csv")
+)), firstBack='TRUE')
+
+
+androidsimplefacebook = colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/android-simple-facebook/android-simple-facebook_commits.csv"), 
+	read.csv("api_results/android-simple-facebook/android-simple-facebook_forks.csv"), 
+	read.csv("api_results/android-simple-facebook/android-simple-facebook_issues.csv"), 
+	read.csv("checkstyle_results/android-simple-facebook.csv")
+)), firstBack='TRUE')
+
+dagger = colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/dagger/dagger_commits.csv"), 
+	read.csv("api_results/dagger/dagger_forks.csv"), 
+	read.csv("api_results/dagger/dagger_issues.csv"), 
+	read.csv("checkstyle_results/dagger.csv")
+)), firstBack='TRUE')
+
+eclipsethemes = colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/eclipse-themes/eclipse-themes_commits.csv"), 
+	read.csv("api_results/eclipse-themes/eclipse-themes_forks.csv"), 
+	read.csv("api_results/eclipse-themes/eclipse-themes_issues.csv"), 
+	read.csv("checkstyle_results/eclipse-themes.csv")
+)), firstBack='TRUE')
+
+facebookandroidsdk = colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/facebook-android-sdk/facebook-android-sdk_commits.csv"), 
+	read.csv("api_results/facebook-android-sdk/facebook-android-sdk_forks.csv"), 
+	read.csv("api_results/facebook-android-sdk/facebook-android-sdk_issues.csv"), 
+	read.csv("checkstyle_results/facebook-android-sdk.csv")
+)), firstBack='TRUE')
+
+golangideaplugin = colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/go-lang-idea-plugin/go-lang-idea-plugin_commits.csv"), 
+	read.csv("api_results/go-lang-idea-plugin/go-lang-idea-plugin_forks.csv"), 
+	read.csv("api_results/go-lang-idea-plugin/go-lang-idea-plugin_issues.csv"), 
+	read.csv("checkstyle_results/go-lang-idea-plugin.csv")
+)), firstBack='TRUE')
+
+guice = colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/guice/guice_commits.csv"), 
+	read.csv("api_results/guice/guice_forks.csv"), 
+	read.csv("api_results/guice/guice_issues.csv"), 
+	read.csv("checkstyle_results/guice.csv")
+)), firstBack='TRUE')
+
+jedis= colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/jedis/jedis_commits.csv"), 
+	read.csv("api_results/jedis/jedis_forks.csv"), 
+	read.csv("api_results/jedis/jedis_issues.csv"), 
+	read.csv("checkstyle_results/jedis.csv")
+)), firstBack='TRUE')
+
+junit = colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/junit/junit_commits.csv"), 
+	read.csv("api_results/junit/junit_forks.csv"), 
+	read.csv("api_results/junit/junit_issues.csv"), 
+	read.csv("checkstyle_results/junit.csv")
+)), firstBack='TRUE')
+
+okhttp = colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/okhttp/okhttp_commits.csv"), 
+	read.csv("api_results/okhttp/okhttp_forks.csv"), 
+	read.csv("api_results/okhttp/okhttp_issues.csv"), 
+	read.csv("checkstyle_results/okhttp.csv")
+)), firstBack='TRUE')
+
+phonegapfacebookplugin = colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/phonegap-facebook-plugin/phonegap-facebook-plugin_commits.csv"), 
+	read.csv("api_results/phonegap-facebook-plugin/phonegap-facebook-plugin_forks.csv"), 
+	read.csv("api_results/phonegap-facebook-plugin/phonegap-facebook-plugin_issues.csv"), 
+	read.csv("checkstyle_results/phonegap-facebook-plugin.csv")
+)), firstBack='TRUE')
+
+retrofit = colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/retrofit/retrofit_commits.csv"), 
+	read.csv("api_results/retrofit/retrofit_forks.csv"), 
+	read.csv("api_results/retrofit/retrofit_issues.csv"), 
+	read.csv("checkstyle_results/retrofit.csv")
+)), firstBack='TRUE')
+
+roboguice = colwise(fillNAgaps)(Reduce(function(...) merge(..., by='time', all=T), list(
+	read.csv("api_results/roboguice/roboguice_commits.csv"), 
+	read.csv("api_results/roboguice/roboguice_forks.csv"), 
+	read.csv("api_results/roboguice/roboguice_issues.csv"), 
+	read.csv("checkstyle_results/roboguice.csv")
+)), firstBack='TRUE')
+
